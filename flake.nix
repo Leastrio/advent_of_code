6c797a167a72bf5420edb70c8d8cf86018dbc81d
Leastrio <jacob@leastr.io>
2024-12-02 00:02:24 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            livebook
+          ];
+
+          shellHook = ''
+            livebook start
+            exit
+          '';
+        };
+      }
+    );
+  }
